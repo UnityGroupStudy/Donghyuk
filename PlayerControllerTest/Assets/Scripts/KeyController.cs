f2f35fb3ac2421f98cb907119a0278557a51c3fc
Liunx <kdh950812@gmail.com>
2019-10-03 18:01:52 +0900

    key controller project init

---
+++ PlayerControllerTest/Assets/Scripts/KeyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    public enum KeyType {
+        none, down, up, ll, dd
+    };
+
+    int keyLength;
+    public float[] keyTimes;
+    public int[] keyValues;
+
+    void Start()
+    {
+        keyLength = (int)System.Enum.GetValues(typeof(KeyCode)).Length;
+
+        keyTimes = new float[keyLength];
+        keyValues = new int[keyLength];
+
+        for(int i=0; i<keyLength; i++) {
+            keyTimes[i] = 0;
+            keyValues[i] = (int)System.Enum.GetValues(typeof(KeyCode)).GetValue(i);
+        }
+    }
+
+    void Update()
+    {
+        for(int i=0; i<keyLength; i++) {
+            if(Input.GetKey((KeyCode)keyValues[i]))
+                keyTimes[i] += Time.deltaTime;
+            if(Input.GetKeyUp((KeyCode)keyValues[i]))
+                keyTimes[i] = 0f;
+        }
+    }
+}
