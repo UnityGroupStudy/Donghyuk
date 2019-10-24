75b5673d56b6d418bfd9edd7c08d8db68cd8fc07
Liunx <kdh950812@gmail.com>
2019-10-24 17:10:33 +0900

    assign 3

---
+++ assign_3/Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Rigidbody rBody;
+    float speed = 4f;
+    
+    float jumpForce = 200f;
+    int jumpCount;
+
+    void Awake() {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float _vertical = Input.GetAxis("Vertical");
+        float _horizontal = Input.GetAxis("Horizontal");
+
+        rBody.MovePosition(transform.position + (Vector3.forward * _vertical + Vector3.right * _horizontal) * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space) && jumpCount < 3) {
+            rBody.velocity = Vector3.zero;
+            rBody.AddForce(Vector3.up * jumpForce);
+            jumpCount += 1;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        jumpCount = 0;
+    }
+}
---
+++ assign_3/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    TextMesh timeText;
+    int timeCount = 90;
+
+
+    void Start()
+    {
+        timeText = GetComponent<TextMesh>();
+        timeText.text = (timeCount/60).ToString() + " : " + (timeCount%60).ToString();
+
+        StartCoroutine(Count());
+    }
+
+    IEnumerator Count() {
+        while(timeCount > 0) {
+            yield return new WaitForSeconds(1f);
+
+            timeCount -= 1;
+
+            timeText.text = (timeCount/60).ToString() + " : " + (timeCount%60).ToString();
+        }
+    }
+}
